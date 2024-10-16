0d09598a8127d493949151f33b5ece4c0768b360
Ramji67 <158837797+Ramji67@users.noreply.github.com>
2024-10-16 23:01:44 +0530

    config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("28088290", ""))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("6998f2c585fdce65ac72dfa23d02b6ec", "")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("mongodb+srv://Ramji:<db_Ramji@2019@ramji.hn1y5.mongodb.net/?retryWrites=true&w=majority&appName=Ramji", "")
